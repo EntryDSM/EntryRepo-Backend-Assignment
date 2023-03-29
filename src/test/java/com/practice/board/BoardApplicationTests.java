e6e7831ac65d6587183c6cdaec70320bf7ca2d03
rlaisqls <rlaisqls@gmail.com>
2023-03-30 00:06:57 +0900

    chore :: 불필요한 테스트코드 삭제
    제가 1학년때 만든 코드를 베이스로 짠거라서 그때 짰던 코드가 같이 들어간 것 같습니다...ㅎ

--- src/test/java/com/practice/board/BoardApplicationTests.java
+++ src/test/java/com/practice/board/BoardApplicationTests.java
@@ -1,24 +1,10 @@
 package com.practice.board;
 
-import com.practice.board.global.security.jwt.JwtProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class BoardApplicationTests {
-
-	@Autowired
-	private JwtProperties jwtProperties;
-
-	@Test
-	public void contextLoad() throws Exception{
-		System.out.println(jwtProperties);
-	}
 
 }
